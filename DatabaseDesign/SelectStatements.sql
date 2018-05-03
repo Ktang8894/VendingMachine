9d4d1e0cca0407c29c3457dfc92a82c4dbf25f10
Kevin Tang <kevin.tang@echo.com>
2018-05-03 15:13:19 -0500

    fixed a few spelling errors, sql statements

--- DatabaseDesign/SelectStatements.sql
+++ DatabaseDesign/SelectStatements.sql
@@ -1,10 +1,11 @@
 --Sour Items
-SELECT Name
+SELECT *
 FROM VendingMachine.Item
 WHERE Flavor = 'SOUR'
+AND StockCount > 0;
 
---Most common Wrapper Color (what if it's tied?)
-SELECT Name
+--Most common Wrapper Color in the Trash Compartment
+SELECT WrapperColor
 FROM VendingMachine.Item
 WHERE TrashCount = (SELECT MAX(TrashCount) FROM VendingMachine.Item)
 
